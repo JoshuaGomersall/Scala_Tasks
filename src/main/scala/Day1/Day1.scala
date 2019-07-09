cb54b5077a55439b3624358b0400e1004783eb85
Joshua <joshuagomersallgebba@gmail.com>
2019-07-09 12:34:14 +0100

    Day1

--- src/main/scala/Day1/Day1.scala
+++ src/main/scala/Day1/Day1.scala
@@ -1,5 +1,32 @@
 package Day1
 
-class Day1 {
+object Day1 {
+    def main(args: Array[String]): Unit = {
+      var helloWorldString : String = "Hello World"
+      println(helloWorldString)
 
+
+      helloWorldPrint(helloWorldString)
+      println(helloWorld("Hello World"))
+
+
+      varibleTypeCheck(4)
+
+
+    }
+
+
+
+
+  def helloWorldPrint (input: String) : Unit = {
+    println(input)
+  }
+
+    def helloWorld (input: String) : String = {
+      input
+    }
+
+  def varibleTypeCheck (input : String) : String = {
+    input
+  }
 }
