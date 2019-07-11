6f7982da4764272b90c940855e190438147a419f
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-11 18:20:11 +0100

    Day1 HelloWorld Code CleanUp Var Changed To Val

--- src/main/scala/Day1/Day1.scala
+++ src/main/scala/Day1/Day1.scala
@@ -2,7 +2,7 @@ package Day1
 
 object Day1 {
   def main(args: Array[String]): Unit = {
-    var helloWorldString: String = "Hello World"
+    val helloWorldString: String = "Hello World"
     println(helloWorldString)
     HelloWorld.helloWorldPrint(helloWorldString)
     println(HelloWorld.helloWorld("Hello World"))
