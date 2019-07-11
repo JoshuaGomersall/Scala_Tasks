61a0dadabb9c764513903a8ee6a23ea9f6420cdf
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-11 22:41:28 +0100

    Day3 Too Hot Checker Equation Shortened

--- src/main/scala/Day3/Day3.scala
+++ src/main/scala/Day3/Day3.scala
@@ -4,9 +4,10 @@ package Day3
 object Day3 {
   def main(args: Array[String]): Unit = {
 
-    println(TooHotChecker.summerHeatChecker(80,false))
-    println(TooHotChecker.summerHeatChecker(100,false))
-    println(TooHotChecker.summerHeatChecker(100,true))
+    println(TooHotChecker.summerHeatChecker(80,isSummer = false))
+    println(TooHotChecker.summerHeatChecker(100,isSummer = false))
+    println(TooHotChecker.summerHeatChecker(100,isSummer = true))
+    println(TooHotChecker.summerHeatChecker(55,isSummer = true))
 
     //HashMapAnagrams.checkReadFromFileExists("random file name")
     //HashMapAnagrams.checkReadFromFileExists("target/scala-2.13/classes/Day3/examplefile.txt")
