24e39276b20d002ff0d83a87d3fe41525544b731
JoshuaGomersall <47025499+JoshuaGomersall@users.noreply.github.com>
2019-07-11 19:40:26 +0100

    Day1 Sum Changed To Match From If,Else

--- src/main/scala/Day1/Sum.scala
+++ src/main/scala/Day1/Sum.scala
@@ -3,11 +3,10 @@ package Day1
 object Sum{
   def add(int: Int, int2: Int): Int = {int + int2}
 
-  def addOrMultiply(int: Int, int2: Int , addCheck: Boolean): Int = {
+  def addOrMultiply(int: Int, int2: Int , addCheck: Boolean): Int =
     addCheck match {
-      case _ if (int == 0 && int == 0) => 0
-      case true => int + int2
-      case false => int * int2
-    }
+    case _ if (int == 0 && int == 0) => 0
+    case true => int + int2
+    case false => int * int2
   }
 }
